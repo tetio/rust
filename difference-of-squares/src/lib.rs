075c3e12fd49507c5bd7eb14027db765050828c3
Sergi <smaymi@portic.net>
2024-10-14 06:47:43 +0200

    diff squares ok

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,14 +1,12 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let sum: u32 = (1..=n).sum(); //   vsum().pow(2);
-    sum.pow(2)
+    let sum = n * (n + 1) / 2;
+    sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).map(|x| x.pow(2)).sum()
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
-    let s = (1..=n).map(|x| (1..=n).map(move |y| (x as i32, y as i32))).flatten().collect::<Vec<(i32, i32)>>();
-    let diff: i32 = s.iter().map(|(x, y)| (x - y).pow(2)).sum();
-    diff as u32 / 2
+    square_of_sum(n) - sum_of_squares(n)
 }
